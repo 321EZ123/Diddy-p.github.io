dae28b53d092d6b0ae75e92806b194e25e163692
NEXTGENGAMING <xxphoeni@icloud.com>
2025-03-03 02:37:25 +0000

    ui update

--- Games.js
+++ Games.js
@@ -1,44 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const searchResults = document.getElementById('searchResults');
     const searchResultTemplate = document.getElementById('searchResultTemplate').innerHTML;
-  
+
     searchResults.innerHTML = 'Games loading...';
-    
-  
-  
+
     fetch('games.json')
         .then(response => response.json())
         .then(data => {
-            const mockData = data; 
-  
-            
             function renderSearchResults(searchTerm = '') {
                 searchResults.innerHTML = '';
-  
-                
-                const filteredResults = mockData.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
-  
-                
+
+                const filteredResults = data.filter(item =>
+                    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+                );
+
                 filteredResults.forEach(result => {
                     const renderedTemplate = renderTemplate(searchResultTemplate, result);
                     searchResults.innerHTML += renderedTemplate;
                 });
+
+                // Attach event listeners to game links
+                document.querySelectorAll('.game-link').forEach(link => {
+                    link.addEventListener('click', function (event) {
+                        event.preventDefault();
+                        const gameUrl = this.getAttribute('href');
+
+                        if (gameUrl) {
+                            window.location.href = `game.html?game=${encodeURIComponent(gameUrl)}`;
+                        } else {
+                            console.error('Game URL is missing');
+                        }
+                    });
+                });
             }
-  
-  
+
             renderSearchResults();
-  
-            
-            searchInput.addEventListener('input', function() {
+
+            searchInput.addEventListener('input', function () {
                 const searchTerm = searchInput.value.trim();
                 renderSearchResults(searchTerm);
             });
         })
         .catch(error => console.error('Error fetching data:', error));
-  });
-  
-  function renderTemplate(template, data) {
+});
+
+function renderTemplate(template, data) {
     return template.replace(/{{(.*?)}}/g, (match, key) => data[key.trim()]);
-  }
-  
+}
